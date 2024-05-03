475a1bdab848d7725f9971d10e3c80cbfcd586ff
Vishakha Saxena <168802276+vishakhasaxena@users.noreply.github.com>
2024-05-03 17:05:39 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,23 @@
+package university.management.system;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "Vishakha");
+            s=c.createStatement();
+                    
+                    
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new Conn();
+    }
+    
+}
+  
